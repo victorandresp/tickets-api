de161d54d21aedaeb0ad8e64bc80e1187cd97d68
victorandresp <ponceottamendi27@gmail.com>
2024-02-27 23:56:30 -0400

    chore: eslint and prettier config :wrench:

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-const testVar: string = 'testt'
-console.log(`${testVar}`)
+const addition = (a: number, b: number): number => {
+    return a + b;
+  }
+  
+  let number1: number = 5;
+  var number2: number = 10;
+  const result: number = addition(number1, number2);
+  
+  console.log("The result is %d", result );
+  
