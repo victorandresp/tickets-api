dc393dabae4f483245a7bf340e743abf01c23bb3
victorandresp <ponceottamendi27@gmail.com>
2024-02-28 22:58:01 -0400

    chore: eslint rule single quotes :wrench:

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,30 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'plugin:prettier/recommended',
-    'prettier',
-    'eslint:recommended'
-  ],
-  plugins: ['@typescript-eslint'],
+  parser: "@typescript-eslint/parser",
+  extends: ["plugin:prettier/recommended", "prettier", "eslint:recommended"],
+  plugins: ["@typescript-eslint"],
   parserOptions: {
     ecmaVersion: 2022,
-    sourceType: 'module',
-    project: 'tsconfig.json',
+    sourceType: "module",
+    project: "tsconfig.json",
   },
   env: {
     es6: true,
     node: true,
   },
   rules: {
-    'no-var': 'error',
-    indent: ['error', 2, { SwitchCase: 1 }],
-    'no-multi-spaces': 'error',
-    'space-in-parens': 'error',
-    'no-multiple-empty-lines': 'error',
-    'prefer-const': 'error',
-    "prettier/prettier": ["error", { "endOfLine": "auto" }]
+    "no-var": "error",
+    "indent": ["error", 2, { SwitchCase: 1 }],
+    "no-multi-spaces": "error",
+    "space-in-parens": "error",
+    "no-multiple-empty-lines": "error",
+    "prefer-const": "error",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+        singleQuote: false,
+        semi: false,
+      },
+    ],
   },
-};
+}
