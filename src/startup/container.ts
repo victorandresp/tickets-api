37d1ea862e4946e02920bc03397f5d9b95579d44
victorandresp <ponceottamendi27@gmail.com>
2024-03-01 23:12:18 -0400

    feat: server initialited :sparkles:

---
+++ src/startup/container.ts
@@ -0,0 +1,16 @@
+import { createContainer, asClass, asValue } from "awilix"
+import config from "../config";
+
+import { Server } from "."
+
+const container = createContainer();
+
+container
+    .register({
+        app: asClass(Server).singleton(),
+        // router: asFunction(Routes).singleton(),
+        config: asValue(config)
+    })
+
+
+export default container
---
+++ src/startup/index.ts
@@ -0,0 +1,20 @@
+import express, { Express } from "express"
+
+let _express: Express | null = null
+let _config: any | null = null
+
+export class Server {
+  constructor({ config }: any) {
+    _config = config
+    _express = express()
+  }
+
+  start() {
+    return new Promise<void>((resolve) => {
+      _express?.listen(_config?.PORT, () => {
+        console.log(`${_config.APP_NAME} API running on port ${_config.PORT}`)
+      })
+      resolve()
+    })
+  }
+}
