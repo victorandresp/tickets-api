e0430be542bbfcd24ab0bb6c280590a9c58f927f
victorandresp <ponceottamendi27@gmail.com>
2024-03-07 23:36:56 -0400

    feat: postgres bbdd connection :sparkles:

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
 import container from "@/startup/container"
+import { Sequelize } from "sequelize"
 
 const server = container.resolve("app")
+const config = container.resolve("config")
 
-server.start()
+export const sequelize = new Sequelize(config.PG_DB, config.PG_USERNAME, config.PG_PASSWORD, {
+  host: config.PG_HOST,
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+})
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.")
+    server.start()
+  })
+  .catch((error) => console.error(error))
