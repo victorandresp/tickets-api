5a8eedede4690cdc66617fea4f9a68f90826af99
victorandresp <ponceottamendi27@gmail.com>
2024-03-07 23:36:35 -0400

    feat: bbdd variables :wrench:

--- src/config/config.interface.ts
+++ src/config/config.interface.ts
@@ -2,4 +2,7 @@ export interface Config {
   APP_NAME: string
   PORT: number
   API_VERSION: string
+  PG_DB: string
+  PG_USERNAME: string
+  PG_PASSWORD: string
 }
--- src/config/index.ts
+++ src/config/index.ts
@@ -7,6 +7,10 @@ const config = {
   APP_NAME: process.env.APP_NAME,
   PORT: process.env.PORT,
   API_VERSION: process.env.API_VERSION,
+  PG_DB: process.env.PG_DB,
+  PG_USERNAME: process.env.PG_USERNAME,
+  PG_PASSWORD: process.env.PG_PASSWORD,
+  PG_HOST: process.env.PG_HOST,
 }
 
 export default config
