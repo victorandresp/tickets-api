d553f741004cea003f3da1fec3d109d31dee3cf0
victorandresp <ponceottamendi27@gmail.com>
2024-03-18 22:54:55 -0400

    feat: user model interface and auth service/controller interfaces :sparkles:

---
+++ src/interfaces/auth.interface.ts
@@ -0,0 +1,15 @@
+import { Response, Request } from "express"
+import { User } from "@/interfaces/user.interface"
+
+export interface AuthController {
+  // eslint-disable-next-line no-unused-vars
+  signIn(req: Request, res: Response): void
+  // eslint-disable-next-line no-unused-vars
+  signUp(req: Request, res: Response): void
+}
+export interface AuthService {
+  // eslint-disable-next-line no-unused-vars
+  signIn(user: User): void
+  // eslint-disable-next-line no-unused-vars
+  signUp(user: User): void
+}
---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: number
+  uid: string
+  firstName: string
+  lastName: string
+  password: string
+  email: string
+  verifiedAccount: boolean
+  createdAt: string
+  updatedAt: string
+}
