5e7a50bc008d0c9e711d7cdeb960c84ef276040e
victorandresp <ponceottamendi27@gmail.com>
2024-03-21 23:33:22 -0400

    feat: respositories injected dependencies :sparkles:

--- src/startup/layer-dependencies.ts
+++ src/startup/layer-dependencies.ts
@@ -2,9 +2,11 @@ import modelsResolver from "@/models/models.resolver"
 import routesResolver from "@/routes/routes.resolver"
 import controllerResolver from "@/controllers/controllers.resolver"
 import servicesResolver from "@/services/services.resolver"
+import repositoriesResolver from "@/repositories/repositories.resolver"
 import container from "./container"
 
 container.register(modelsResolver)
 container.register(routesResolver)
 container.register(controllerResolver)
 container.register(servicesResolver)
+container.register(repositoriesResolver)
