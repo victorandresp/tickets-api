643d5b66c4d53ff88f4543508bf43f0d26ee82f4
victorandresp <ponceottamendi27@gmail.com>
2024-04-09 23:32:52 -0400

    feat: injected user Repository and Services dependencies :syringe:

--- src/repositories/repositories.resolver.ts
+++ src/repositories/repositories.resolver.ts
@@ -1,9 +1,11 @@
 import { asClass } from "awilix"
 
 import AuthRepository from "@/repositories/auth/auth.repository"
+import UserRepository from "@/repositories/user/user.repository"
 
 const repositoriesResolver = {
   AuthRepository: asClass(AuthRepository).singleton(),
+  UserRepository: asClass(UserRepository).singleton(),
 }
 
 export default repositoriesResolver
--- src/services/services.resolver.ts
+++ src/services/services.resolver.ts
@@ -1,9 +1,11 @@
 import { asClass } from "awilix"
 
 import AuthService from "@/services/auth/auth.service"
+import UserService from "@/services/user/user.service"
 
 const servicesResolver = {
   AuthService: asClass(AuthService).singleton(),
+  UserService: asClass(UserService).singleton(),
 }
 
 export default servicesResolver
