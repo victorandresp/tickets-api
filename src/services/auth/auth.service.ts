401f4f6a6c1b8b4323e3f2ee72ab54085f185736
victorandresp <ponceottamendi27@gmail.com>
2024-04-09 23:34:15 -0400

    feat: get user by email for validation on sign up :sparkles:

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -1,22 +1,28 @@
 import { User } from "@/interfaces/user.interface"
 import { AuthRepository } from "@/interfaces/auth.interface"
+import { UserService } from "@/interfaces/user.interface"
 
 import { ThrowHttpError } from "@/helpers/http.helpers"
 import { isValidEmail } from "@/helpers/user.helpers"
 
 let authRepository: AuthRepository
+let userService: UserService
 
 interface AuthServiceDependencies {
   AuthRepository: AuthRepository
+  UserService: UserService
 }
 class AuthService {
-  constructor({ AuthRepository }: AuthServiceDependencies) {
+  constructor({ AuthRepository, UserService }: AuthServiceDependencies) {
     authRepository = AuthRepository
+    userService = UserService
   }
 
   async signUp(user: User) {
     if (!user.firstName || !user.lastName || !user.email) return ThrowHttpError(400, "Bad request")
     if (!isValidEmail(user.email)) return ThrowHttpError(400, "Enter a valid email")
+    const userRegister = await userService.getUserByEmail(user.email) // TODO: Validate if email exists
+    console.log("userRegister", userRegister)
     return await authRepository.create(user)
   }
   async signIn(user: User) {
