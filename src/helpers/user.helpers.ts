612d591e283449788b7cc7c86a34b11dc6e92caa
victorandresp <ponceottamendi27@gmail.com>
2024-04-11 22:43:43 -0400

    feat: password validation in sign up :sparkles:

--- src/helpers/user.helpers.ts
+++ src/helpers/user.helpers.ts
@@ -2,3 +2,7 @@ export function isValidEmail(email: string) {
   const emailRegEx = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
   return emailRegEx.test(email)
 }
+export function isValidPassword(password: string) {
+  const passRegEx = /^[A-Za-z0-9]{6,}$/
+  return passRegEx.test(password)
+}
--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { AuthRepository } from "@/interfaces/auth.interface"
 import { UserService } from "@/interfaces/user.interface"
 
 import { ThrowHttpError } from "@/helpers/http.helpers"
-import { isValidEmail } from "@/helpers/user.helpers"
+import { isValidEmail, isValidPassword } from "@/helpers/user.helpers"
 
 let authRepository: AuthRepository
 let userService: UserService
@@ -21,6 +21,7 @@ class AuthService {
   async signUp(user: User) {
     if (!user.firstName || !user.lastName || !user.email) return ThrowHttpError(400, "Bad request")
     if (!isValidEmail(user.email)) return ThrowHttpError(400, "Enter a valid email")
+    if (!isValidPassword(user.password)) return ThrowHttpError(400, "Enter a valid password")
     const userRegister = await userService.getUserByEmail(user.email)
     if (userRegister) return ThrowHttpError(409, "Email is already registered")
     return await authRepository.create(user)
