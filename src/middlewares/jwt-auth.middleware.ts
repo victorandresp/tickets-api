82579567122951b71b1bdda51798ca444ca58c51
victorandresp <ponceottamendi27@gmail.com>
2024-04-21 22:30:54 -0400

    feat: jwt auth middleware :sparkles:

---
+++ src/middlewares/jwt-auth.middleware.ts
@@ -0,0 +1,13 @@
+import { Response, Request, NextFunction } from "express"
+import { ThrowHttpError } from "@/helpers/http.helpers"
+import jwt from "jsonwebtoken"
+
+export default function (req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers["authorization"]
+  const token = authHeader && authHeader.split(" ")[1]
+  if (!token) return ThrowHttpError(401, "Unauthorized")
+  jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any) => {
+    if (err) return res.sendStatus(403)
+    next()
+  })
+}
