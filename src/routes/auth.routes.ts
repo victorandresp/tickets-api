bafdd3e9bfb1c0fe39e0cffa038e3def1e493546
victorandresp <ponceottamendi27@gmail.com>
2024-04-22 22:46:53 -0400

    chore: test endpoint for token jwt :test_tube:

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express"
 
 import { AuthController as AuthControllerInterface } from "@/interfaces/auth.interface"
+import JwtAuth from "@/middlewares/jwt-auth.middleware"
 
 interface AuthRoutesDependencies {
   AuthController: AuthControllerInterface
@@ -11,6 +12,9 @@ export default function ({ AuthController }: AuthRoutesDependencies) {
 
   router.post("/signup", AuthController.signUp)
   router.post("/signin", AuthController.signIn)
+  router.post("/test", [JwtAuth], (req: any, res: any) => {
+    return res.status(200).send("Its a valid session")
+  })
 
   return router
 }
