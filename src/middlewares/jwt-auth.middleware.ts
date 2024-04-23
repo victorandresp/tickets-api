a1b4b84b8a7ac571f70e64e361f40548770f19ce
victorandresp <ponceottamendi27@gmail.com>
2024-04-22 22:47:42 -0400

    fix: added correct enviroment  variable :bug:

--- src/middlewares/jwt-auth.middleware.ts
+++ src/middlewares/jwt-auth.middleware.ts
@@ -6,7 +6,7 @@ export default function (req: Request, res: Response, next: NextFunction) {
   const authHeader = req.headers["authorization"]
   const token = authHeader && authHeader.split(" ")[1]
   if (!token) return ThrowHttpError(401, "Unauthorized")
-  jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any) => {
+  jwt.verify(token, process.env.JWT_SECRET as string, (err: any) => {
     if (err) return res.sendStatus(403)
     next()
   })
