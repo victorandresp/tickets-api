006438e72092058383fcfd2d63baf140071a9ed2
victorandresp <ponceottamendi27@gmail.com>
2025-02-04 22:02:49 -0400

    feat: added data aditional for errors :sparkles:

--- src/helpers/http.helpers.ts
+++ src/helpers/http.helpers.ts
@@ -1,10 +1,12 @@
 interface ErrorHttp extends Error {
   status: number
   message: string
+  data?: object
 }
-export function ThrowHttpError(code: number = 500, message: string = "Server internal error") {
+export function ThrowHttpError(code: number = 500, message: string = "Server internal error", data?: object) {
   const error = new Error() as ErrorHttp
   error.status = code
   error.message = message
+  if(data) error.data = data
   throw error
 }
